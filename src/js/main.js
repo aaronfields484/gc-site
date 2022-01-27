ecc38c61af3aa25af22468fa7d8c0f055faa24f3
adrianon24 <ontiveros.on24@gmail.com>
2022-01-27 10:37:02 -0800

    added about page and css

--- src/js/main.js
+++ src/js/main.js
@@ -81,3 +81,42 @@ function openModal() {
     dots[slideIndex-1].className += " active";
     captionText.innerHTML = dots[slideIndex-1].alt;
   }
+
+/*	about gallery */
+
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            $('.filter').show('1000');
+        }
+        else
+        {
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+
+	        	if ($(".filter-button").removeClass("active")) {
+			$(this).removeClass("active");
+		    }
+		    	$(this).addClass("active");
+	    	});
+});
+
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+
+$(document).ready(function(){
+    $(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+    });
+});
+
+/*	end about gallery */
